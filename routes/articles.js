c7f42e3b2a96357f8dd19b6d022624125bba9808
juniorbuking <tgb4085@autuni.ac.nz>
2020-10-06 13:17:48 +1300

    V3(complete)

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,11 @@ router.get('/new', (req, res) => {
     res.render('articles/new', { article: new Article()})
 })
 
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: article })
+})
+
 router.get('/:slug', async (req, res) => {
     const article = await Article.findOne({ slug: req.params.slug })
     if (article == null) res.redirect('/')
@@ -14,23 +19,15 @@ router.get('/:slug', async (req, res) => {
 })
 
 //This is called when a from is submitted
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect ('new'))
 
-    })
-
-    try {
-        article = await article.save()
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        console.log(e)
-        res.render('articles/new', { article: article})
-
-    }
-})
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect ('edit'))
 
 
 method="GET"
@@ -39,5 +36,21 @@ router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+    
+        try {
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            res.render(`articles/${path}`, { article: article})
+        }
+    }
+}
 
 module.exports = router
