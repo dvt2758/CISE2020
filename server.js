25e6a6c2eae2fc6bb418c85da2f8da9ee16807ab
BenZ <dvt2758@autuni.ac.nz>
2020-10-12 23:24:42 +1300

    app update

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
+const indexRouter = require('./routes/index')
 
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
 
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
+app.use('/', indexRouter)
 
-
-app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({createdAt: 'desc'})         //Puts the most recent created articles on top
-    res.render('articles/index', { articles: articles })
-})
-
-app.use('/articles', articleRouter)
-app.listen(5000) 
+app.listen(process.env.PORT || 3000)
